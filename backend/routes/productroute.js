84754217529d9595ba6b1c8b275361ec8c218afc
abhishek saini <abhishek816605@gmail.com>
2026-08-18 14:06:59 +0530

    Add get all products API

--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -19,4 +19,20 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.get("/" , async(req,res) =>{
+    try{
+        const products = await Product.find();
+
+        res.status(200).json({
+            count: products.length,
+            products
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to fetch Products",
+            error: error.message
+        });
+    }
+})
+
 module.exports = router;
