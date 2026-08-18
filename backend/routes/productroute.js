9016a5d6a095697ecd2465f5c685d4d6bc59fd0f
abhishek saini <abhishek816605@gmail.com>
2026-08-18 14:15:04 +0530

    Add get product by ID API

--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -35,4 +35,25 @@ router.get("/" , async(req,res) =>{
     }
 })
 
+router.get("/:id" , async(req,res) =>{
+    try{
+        const product = await Product.findById(req.params.id);
+
+        if(!product){
+            res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json({
+            product
+        });
+    } catch(error) {
+        res.status(500).json({
+            message: "Failed to fetch Prdouct",
+            error : error.message
+        });
+    }
+})
+
 module.exports = router;
