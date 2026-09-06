2d8e5911700802f81be9c4ba94e56e5bcacd5f31
abhisheks0001 <abhishek816605@gmail.com>
2026-09-06 17:04:21 +0530

    Delete frontend directory

--- frontend/lib/core/constants/api_constants.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'http://localhost:8000';
-
-  static const String products = '/api/products';
-  static const String admin = '/api/admin';
-  static const String customer = '/api/customer';
-}
